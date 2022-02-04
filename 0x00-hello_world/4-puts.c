f6997263db798da1f44f51ffa52a039b9c23dd69
METC <metc_ck1@hotmail.com>
2022-02-03 20:22:03 -0800

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  * Description: Utilizing Betty style code
  *
  */
-
-int main(void) /* Description: Main */
+int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
 
