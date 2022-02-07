0dba1775ebf867564459a1a9f824331b43512efc
METC <metc_ck1@hotmail.com>
2022-02-06 21:24:29 -0800

    fourth

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Function to print alphabet minus q or e 
+ *
+ *Return: Zero
+ */
+int main(void)
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+	if (letra != 'e' || letra != 'q')
+		{
+		putchar (letra);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
