998aca19e392b8e458f883762abb3fc9bca4e689
METC <metc_ck1@hotmail.com>
2022-02-06 20:53:21 -0800

    third

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function to print alphabet
+ *
+ *Return: Zero
+ */
+int main(void)
+{
+	char letra;
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+	putchar (letra);
+	}
+	putchar ('\n');
+	return (0);
+}
