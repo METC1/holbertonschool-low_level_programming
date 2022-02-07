200e03408787bb109ae4867a6cdeabe5c8da0efc
METC <metc_ck1@hotmail.com>
2022-02-06 23:12:48 -0800

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function to print alphabet minus q or e
+ *
+ *Return: Zero
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = 0; numero < 10; numero++)
+	{
+		printf ("%i", numero);
+	}
+	printf ("\n");
+	return (0);
+}
