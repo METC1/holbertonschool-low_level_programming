fdd4e975afd5ddc752ee3136fa7766046f89a0ea
METC <metc_ck1@hotmail.com>
2022-02-06 19:27:05 -0800

    first

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Function to test if a number is greater than zero, 
+ *equal to zero or less than zero
+ *
+ *Return: Zero
+ */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	}
+	else if (n < 0)
+	{
+		printf("Last digit of %d is %d and is lees than 6 and not 0\n", n, last);
+	}
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
+	
+	return (0);
+}
