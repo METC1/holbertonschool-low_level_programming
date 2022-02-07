a6d0fa3460985963cb6ad184ba2c4d0f170cd029
METC <metc_ck1@hotmail.com>
2022-02-06 23:28:40 -0800

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function to print alphabet
+ *
+ *Return: Zero
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = 48; numero < 58; numero++)
+	{
+	putchar (numero);
+	}
+	putchar ('\n');
+	return (0);
+}
