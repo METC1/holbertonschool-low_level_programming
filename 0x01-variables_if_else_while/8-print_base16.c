7a60696a8d69316117b2d12024602d8dbebae286
METC <metc_ck1@hotmail.com>
2022-02-07 21:08:51 -0800

    8 ver 3

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,73 +9,13 @@ int main(void)
 	int numero;
 	char hexa;
 
-	for (numero = 0; numero <= 15; numero++)
+	for (numero = '0'; numero <= '9'; numero++)
 	{
-		if (numero == 0)
-		{
-		hexa = '0';
-		}
-		else if (numero == 1)
-		{
-		hexa = '1';
-		}
-		else if (numero == 2)
-		{
-		hexa = '2';
-		}
-		else if (numero == 3)
-		{
-		hexa = '3';
-		}
-		else if (numero == 4)
-		{
-		hexa = '4';
-		}
-		else if (numero == 5)
-		{
-		hexa = '5';
-		}
-		else if (numero == 6)
-		{
-		hexa = '6';
-		}
-		else if (numero == 7)
-		{
-		hexa = '7';
-		}
-		else if (numero == 8)
-		{
-		hexa = '8';
-		}
-		else if (numero == 9)
-		{
-		hexa = '9';
-		}
-		else if (numero == 10)
-		{
-		hexa = 'a';
-		}
-		else if (numero == 11)
-		{
-		hexa = 'b';
-		}
-		else if (numero == 12)
-		{
-		hexa = 'c';
-		}
-		else if (numero == 13)
-		{
-		hexa = 'd';
-		}
-		else if (numero == 14)
-		{
-		hexa = 'e';
-		}
-		else if (numero == 15)
-		{
-		hexa = 'f';
-		}
-		putchar (hexa);
+	putchar (numero);
+	}
+	for (numero = 'a'; numero <='f'; numero++)
+	{
+	putchar (numero);
 	}
 	putchar ('\n');
 	return (0);
