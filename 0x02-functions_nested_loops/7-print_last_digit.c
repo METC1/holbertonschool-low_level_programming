77de997935a80ea1c9d8668b1f4cf3f03a1e9d4e
METC <metc_ck1@hotmail.com>
2022-02-08 21:43:27 -0800

    7-last digit v3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@
  */
 int print_last_digit(int n)
 {
-	int lastd = 0;
+int lastd = 0;
 
-	lastd = n % 10;
-       if (lastd < 0)
-       lastd = (-1 * lastd);
-	_putchar(lastd + '0');       
-	return (lastd);
+lastd = n % 10;
+if (lastd < 0)
+lastd = (-1 * lastd);
+_putchar(lastd + '0');
+return (lastd);
 }
