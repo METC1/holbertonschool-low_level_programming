48c67d4e7d3f1439cbf642428905e926097a0252
METC <metc_ck1@hotmail.com>
2022-02-08 21:22:58 -0800

    6-abs v1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Function to compute abs of a number
+ * @n: n is a number to be checked
+ *
+ * Return: Abs of number
+ */
+int print_sign(int n)
+{
+	int return = 0;
+
+	if (n <0)
+	n = -1 * n;
+	return (n);
+}
