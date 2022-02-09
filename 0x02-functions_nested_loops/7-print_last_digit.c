f9f1f460dfe9c37340a9f110127438a5e253b752
METC <metc_ck1@hotmail.com>
2022-02-08 21:35:17 -0800

    7-last digit v1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function to compute abs of a number
+ * @n: n is a number to be checked
+ *
+ * Return: last digit of number
+ */
+int print_last_digit(int n)
+{
+	int lastd = 0;
+
+	lastd = n % 10 
+	return (lastd);
+}
