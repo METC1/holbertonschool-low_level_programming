364e624d0811a23e8229d8fd54420aad097af6e1
METC <metc_ck1@hotmail.com>
2022-02-08 18:48:20 -0800

    1-alphabet v6

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * main - Function to print alphabet using _putchar
- *
- *Return: Zero
+ * print_alphabet - Function to print alphabet using _putchar
+ * 
+ * Return: Void
  */
 void print_alphabet(void)
 {
 	int letra;
 	
-	for (letra = 'a'; letra <='z'; letra++)
+	for (letra = 'a'; letra <= 'z'; letra++)
 	{
 	_putchar(letra);
 	}
