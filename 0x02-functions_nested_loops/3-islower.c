cb1e18c864c1e78a3acd8b9ce933b8eac2f3aa0e
METC <metc_ck1@hotmail.com>
2022-02-08 19:15:13 -0800

    3-islower v1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Function to print alphabet using _putchar
+ *
+ * Return: Int 0 if not lowercase 1 if lowercase
+ */
+int _islower(int c)
+{
+int check = 0;
+
+if (islower(c) == 0)
+	check = 1;
+}
+return (check);
+}
