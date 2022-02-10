2331608b5bb788d31f38b7b5bf3d999b31ca2df6
METC <metc_ck1@hotmail.com>
2022-02-09 20:13:57 -0800

    9-times table v1

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Function to print times tables
+ *
+ *Return: Void
+ */
+void times_table(void)
+{
+	int n1;
+	int n2;
+	int res;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0 ; n2 <= 9; n2++)
+		{
+			
+				
+				res = n1 * n2;
+				if (res < 9)
+				{
+				_putchar(' ');
+				}
+				_putchar(' ');
+				_putchar(res + '0');
+				if (n2 < 9)
+				{	
+				_putchar(',');
+				}
+		}
+	}
+	_putchar('\n');
+}
+
