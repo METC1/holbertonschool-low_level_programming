968a1568dbe978527215d221e32b319af7921227
METC <metc_ck1@hotmail.com>
2022-02-09 20:29:30 -0800

    9-times table v3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,21 +11,21 @@ void times_table(void)
 	int res;
 
 	for (n1 = 0; n1 <= 9; n1++)
-	{
-	for (n2 = 0 ; n2 <= 9; n2++)
 		{
-				res = n1 * n2;
-				if (res < 9)
-				{
-				_putchar(' ');
-				}
-				_putchar(' ');
-				_putchar(res + '0');
-				if (n2 < 9)
-				{
+		for (n2 = 0 ; n2 <= 9; n2++)
+			{
+			res = n1 * n2;
+			if (res < 9)
+			{
+			_putchar(' ');
+			}
+			_putchar(' ');
+			_putchar(n1 + '0');
+			if (n2 < 9)
+			{
 				_putchar(',');
-				}
+			}
+			}
 		}
-	}
 	_putchar('\n');
 }
