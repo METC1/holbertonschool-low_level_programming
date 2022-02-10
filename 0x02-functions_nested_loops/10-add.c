4ea5bb332c8eff0244f7b552e2e89be3ddd5a4db
METC <metc_ck1@hotmail.com>
2022-02-09 21:01:35 -0800

    10-add v1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Function to add 2 int numbers
+ *
+ * Return: Int addition of two numbers
+ */
+int add(int a, int b)
+{
+	int added = 0;
+
+	added = a + b;
+	return (added);
+}
