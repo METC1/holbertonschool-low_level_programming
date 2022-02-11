01f640c1d7ff52d8f6eaf4fd2b60d43c57c67218
METC <metc_ck1@hotmail.com>
2022-02-10 22:01:47 -0800

    3 last?

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0) && (year % 400 == 0 && year %100 ==0))
+if ((year % 4 == 0) && (year % 400 == 0 && year % 100 == 0))
 {
 	if (month >= 2 && day >= 60)
 	{
@@ -30,5 +30,4 @@ else if (month == 2 && day == 60)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 			}
-		
 }
