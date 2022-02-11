a40ad1b71c0c8195e89d8258f0001bfef1bf2601
METC <metc_ck1@hotmail.com>
2022-02-11 11:01:17 -0800

    9-fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * FizzBuzz - print numbers from 1 to 100 but replaces multiples of 3 or 5
+ * Description: Print Fizz Buzz
+ * @n: int
+ * Return: void
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (!(i % 3 == 0 || i % 5 == 0))
+			printf("%d", i);
+	if (i < 100)
+	printf(" ");
+	}
+}
