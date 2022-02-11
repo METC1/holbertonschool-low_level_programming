e764553be52ca108bd14fc01be25f51218fccdea
METC <metc_ck1@hotmail.com>
2022-02-10 18:26:19 -0800

    2-largest v5

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a >= b && a >= c)
 	{
 	largest = a;
 	}
@@ -20,7 +20,7 @@ else if (b > a && b > c)
 	{
 	largest = b;
 	}
-else if (c > a && c > b)
+else
 	{
 	largest = c;
 	}
