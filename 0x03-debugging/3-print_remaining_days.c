6e3b9f1c31fc85de3c27953e6172ec5754a40684
METC <metc_ck1@hotmail.com>
2022-02-11 09:29:31 -0800

    3 again

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,9 +20,11 @@ if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
 	}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-	
+
 }
-else if (month == 2 && day == 60)
+else
+{
+	if (month == 2 && day == 60)
 	{
 	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
@@ -31,4 +33,5 @@ else if (month == 2 && day == 60)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 			}
+}
 }
