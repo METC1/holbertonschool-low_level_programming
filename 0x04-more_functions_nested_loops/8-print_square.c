c19efe40be449ab62eeeaad4f134fca6fde08730
METC <metc_ck1@hotmail.com>
2022-02-10 21:21:41 -0800

    8-square v1

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,6 +8,12 @@
  */
 void print_square(int size)
 {
+	if (size <0 )
+	{
+		_putchar(10);
+	}
+	else
+	{
 	int i, j;
 
 		for (i = 0 ; i < size ; i++)
@@ -18,4 +24,5 @@ void print_square(int size)
 			}
 		_putchar(10);
 		}
+	}
 }
