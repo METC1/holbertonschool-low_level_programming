b308152e47e9db50f487aec6eb190e21357fe65c
METC <metc_ck1@hotmail.com>
2022-02-14 21:05:29 -0800

    1-swap ints v1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int -  swap ints with pointers
+ * @a: First int (its adresss)to be swapped 
+ * @b: Second int (its adress) to be swapped
+ * Return: Void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
