57f0b5ea0b58b679be940ee54a8982b34a97577f
METC <metc_ck1@hotmail.com>
2022-02-15 14:40:38 -0800

    5-rev_string v1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * Description: Reversea string in the same variable
+ * @s: char array to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+	i = 0;
+while (*s[i] != '\0')
+	i++
+for (j = 0; j == i/2; j++)
+	{
+	temp = *s[j];
+	*s[j] = *s[i - j];
+	*s[i -j] = temp;
+	}
+}
