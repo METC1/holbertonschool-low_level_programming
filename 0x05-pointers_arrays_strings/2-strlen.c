7ae5cde5df896ed57ece0969e0d415e1aee6d7d7
METC <metc_ck1@hotmail.com>
2022-02-14 21:26:12 -0800

    2-strlen v1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen -  returns lenght of string
+ * @s: string array adress to be measured
+ * Return: int The lenght of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	char j;
+
+	for (i = 0; j == '\0'; i++)
+	{
+		j = *(s + i);
+	}
+	return (i);
+}
