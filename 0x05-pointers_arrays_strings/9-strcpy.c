6f78ab7389806fb3094573b17cae78de6c0e19c3
METC <metc_ck1@hotmail.com>
2022-02-15 19:33:34 -0800

    9-strcpy v1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strcpy - Copies a string pointed to a buffer pointed
+* @dest: buffer to copy to
+* @src: string to be copied
+* Return: char pointer to the copied string holder
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i, j = 0;
+
+while (src[i] != '\0')
+	i++;
+for (j = 0; j <= i; j++)
+{
+	dest[j] = src[j];
+}
+	return (dest);
+}
