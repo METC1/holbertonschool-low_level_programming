c7f7e801b4ec5a295b042dacfbf84e3018c82cd1
METC <metc_ck1@hotmail.com>
2022-02-15 19:20:22 -0800

    sixth v3

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
-* puts2 - prints every other character of a string
-* @str: char string source
+* puts2 - prints every other character of a string, followed by a new line
+* @str: char
 * Return: void
 */
 
 void puts2(char *str)
 {
-int i, j;
+	int a, b;
 
-while (str[i] != '\0')
-	i++;
-i--;
-for (j = 0 ; j < i ; j += 2)
-{
-	_putchar(str[j]);
-}
-_putchar('\n');
+	for (a = 0 ; str[a] != '\0' ; a++)
+	{
+	}
+	b = a;
+	for (b = 0 ; b < a ; b += 2)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
 }
