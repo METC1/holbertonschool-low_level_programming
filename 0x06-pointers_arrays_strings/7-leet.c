500d6f53d83347933e1fc8f51edb9c4cfc1bebe3
METC <metc_ck1@hotmail.com>
2022-02-17 21:19:33 -0800

    7-leet v1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337 changing some letters to numbers
+ * @str: char string
+ * Return: char of the string encoded
+ */
+char *leet(char *s)
+{
+int i, j;
+char let1[] = "aeotlAETOL";
+char subst[] = "43071430714";
+
+for (i = 0 ; s[i] != '\0' ; i++)
+{
+	for (j = 0 ; j <= 9 ; j++)
+	{
+		if (s[i] == let1[j])
+		{
+			s[i] = subst[j];
+		}
+	}
+}
+return (s);
+}
