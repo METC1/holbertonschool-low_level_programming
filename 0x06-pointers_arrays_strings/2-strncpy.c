b24fec3ad3672de52c1aa4bab7e048d939d2fa9c
METC <metc_ck1@hotmail.com>
2022-02-17 20:34:50 -0800

    2-strncpy v2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,8 +11,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 i = 0;
-while i < n && src[i] != '\0')
-	i++
+while (i < n && src[i] != '\0')
+	i++;
 for ( ; i < n; i++)
 {
 	dest[i] = '\0';
