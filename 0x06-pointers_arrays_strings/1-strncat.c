c6051a99c1baa4eb630310207adf965b9415a524
METC <metc_ck1@hotmail.com>
2022-02-17 20:18:45 -0800

    1-strncat v1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _strcat - Copies a string to other source string
+* _strncpy - Copies a string to other source string
 * @dest: Destination string and to wich must be copied
 * @src: string to append to dest
 * @n: n bytes to be copied from src
@@ -10,12 +10,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+i = 0;
 while (i < n && src[i] != '\0')
 	i++;
 for ( ; i < n; i++)
 {
 	dest[i] = '\0';
 }
-
 return (dest);
 }
