e08a1ef212bb0f3abe32b64ef6c8adef10bfb5c7
METC <metc_ck1@hotmail.com>
2022-02-18 18:19:52 -0800

    4-reverse array v 2

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - Reverses an array
+ * Description: Reverse an array
+ * @a: char array to be reversed
+ * @n: number of elements to swap
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int j;
+	int temp;
+n = n - 1;
+for (j = 0; j <= n / 2; j++)
+	{
+	temp = a[j];
+	a[j] = a[n - j];
+	a[n - j] = temp;
+	}
+}
