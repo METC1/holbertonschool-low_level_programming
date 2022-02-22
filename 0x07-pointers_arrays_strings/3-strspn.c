684fdd829041f9b52f62de1a40611128e5c49b34
METC <metc_ck1@hotmail.com>
2022-02-21 22:02:02 -0800

    3-strspn not working

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - finds how many characters are present in the string 
+ * and the sub string
+ * @s: string to find substring character 
+ * @accept:string or list of characters to search if they exist on the string
+ * Return: number of characters that are in the substring and the string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+unsigned int k;
+
+k = 0;
+for (i = 0; accept[i] != '\0'; i++)
+{
+	for (j = 0; s[j] != '\0'; i++)
+	{
+		if ( accept[i] == s[j])
+		{
+			k = k + 1;
+		}
+	}
+}
+return(k);
+}
