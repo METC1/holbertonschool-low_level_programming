436301fe536da0b806783b18528c3020d9e072c4
METC <metc_ck1@hotmail.com>
2022-02-22 15:28:13 -0800

    3-strspn v5

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _strspn - finds how many characters are present in the string 
+ * _strspn - finds how many characters are present in the string
  * and the sub string
- * @s: string to find substring character 
+ * @s: string to find substring character
  * @accept:string or list of characters to search if they exist on the string
  * Return: number of characters that are in the substring and the string
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
+int i, j;
 unsigned int k;
 
 k = 0;
@@ -16,12 +17,14 @@ for (i = 0; accept[i] != '\0'; i++)
 {
 	for (j = 0; s[j] != '\0'; j++)
 	{
-		if ( accept[i] == s[j])
+		printf("%c", s[j]);
+		if (accept[i] == s[j])
 		{
+			printf("%c", accept[i]);
 			k = k + 1;
 			break;
 		}
 	}
 }
-return(k);
+return (k);
 }
