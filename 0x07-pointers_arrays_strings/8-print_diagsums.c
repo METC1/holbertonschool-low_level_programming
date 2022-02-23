d8b766d7ec5d5aee9f0a7b44a2db8d361c5d42de
METC <metc_ck1@hotmail.com>
2022-02-22 21:57:20 -0800

    8sum diag v1

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the diagonal sums of an array
+ * @a: array of ints
+ *Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+int i, d1, d2;
+
+d1 = 0;
+d2 = 0;
+for (i = 0; i < size; i++)
+{
+	d1 = d1 + a[(i * size) + i];
+	d2 = d2 + a[(size - 1) * (i + 1)];
+}
+printf("%i, %i\n", d1, d2);
+}
