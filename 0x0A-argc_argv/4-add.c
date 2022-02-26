3834ae9bf4c53441368f52e57832547e4ebc9d32
METC <metc_ck1@hotmail.com>
2022-02-25 22:23:32 -0800

    4-add fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,23 +8,28 @@
  */
 int main(int argc, char *argv[])
 {
-int i, j = 0;
+int i, j, k = 0;
 int sum = 0;
+char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 if (argc <= 1)
 {
 	printf("0\n");
 	return (0);
 }
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 	for (j = 0; argv[i][j]; j++)
 	{
-		if (argv[i][j] < 48 && argv[i][j] > 57)
-	{
-		printf("Error\n");
-		return (1);
-	}
+		for (k = 0;k < 52; k++)
+		{
+		if (argv[i][j] == alpha[k])
+		{
+			printf("Error\n");
+			return (1);
+		}	
+		}
+	
 	}
 sum = sum + atoi(argv[i]);
 }
