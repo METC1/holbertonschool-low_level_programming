3ffae50e84257882854fa36bbee425a11cc3fe6f
METC <metc_ck1@hotmail.com>
2022-03-02 11:28:49 -0800

    100-atoi betty corrected

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,21 +10,21 @@ int num = 0;
 int i = 0;
 int sign = 1;
 
-for(i = 0; s[i]; i++)
+for (i = 0; s[i]; i++)
 {
-	if(s[i] == '-')
+	if (s[i] == '-')
 	{
 		sign = sign * -1;
-	} 
-	else if(s[i] - '0' >= 0 && s[i] - '0' <= 9)
-		{
-			num = ((num * 10) + (s[i] - '0'));
-		}
-	else if(num > 0)
-		{
-			break;
-		}
-	
+	}
+	else if (s[i] - '0' >= 0 && s[i] - '0' <= 9)
+	{
+	num = ((num * 10) + (s[i] - '0'));
+	}
+	else if (num > 0)
+	{
+	break;
+	}
+
 }
 return (num * sign);
 }
