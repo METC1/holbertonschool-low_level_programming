f423029267c83eeff0e42fa08a7c24ea639f12f3
METC <metc_ck1@hotmail.com>
2022-03-02 11:14:06 -0800

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - extracts a number from a string with infinite character at begining
+ * @s: string array to check
+ *Return: int number in the string with its sign
+ */
+int _atoi(char *s)
+{
+int num = 0;
+int i = 0;
+int sign = 1;
+
+for(i = 0; s[i]; i++)
+{
+	if(s[i] == '-')
+	{
+		sign = sign * -1;
+	} 
+	else if(s[i] - '0' >= 0 && s[i] - '0' <= 9)
+		{
+			num = ((num * 10) + (s[i] - '0'));
+		}
+	else if(num > 0)
+		{
+			break;
+		}
+	
+}
+return (num * sign);
+}
