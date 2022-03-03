6b6cc22668809b0049a3542e1879f5bdfd0b826e
METC <metc_ck1@hotmail.com>
2022-03-02 19:18:19 -0800

    0-create array betty fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * *create_array - Creattes array reserving 
+ * *create_array - Creattes array reserving
  * memmory with malloc and initializes array with given string
  * @size: size of the array
  * @c: char to initialize the array with
