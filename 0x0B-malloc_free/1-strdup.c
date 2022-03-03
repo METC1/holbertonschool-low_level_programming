7dcfa075138fa87eaf5f652d8df824a9fb9dfc0b
METC <metc_ck1@hotmail.com>
2022-03-02 19:45:09 -0800

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_strdup - Reserves memmory and puts string in it
+ * @str: Pointer to the original string
+ *Return: char pointer to the array
+ */
+char *_strdup(char *str)
+{
+int i, j = 0;
+char *str2;
+
+	for (i = 0; str[i]; i++)
+	{
+	}
+	str2 = malloc(sizeof(char) * i);
+	if (str2 == NULL)
+{
+	return (NULL);
+}
+for (j = 0; j < i; j++)
+{
+	str2[j] = str[j];
+}
+return (str2);
+	}
