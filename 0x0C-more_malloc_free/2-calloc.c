49dac17ae1ce466f2d3084d1602020ceae2b43a3
METC <metc_ck1@hotmail.com>
2022-03-03 20:54:31 -0800

    2-calloc v1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_calloc - Creattes array reserving
+ * memmory with malloc and initializes array with 0
+ * @nmemb: elements of the array
+ * @size: size of elements
+ *Return: char pointer to the array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *arr;
+if (size <= 0 || nmemb <= 0)
+{
+	return (NULL);
+}
+arr = malloc(nmemb * size);
+if (arr == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+	arr[i] = 0;
+}
+return (arr);
+}
