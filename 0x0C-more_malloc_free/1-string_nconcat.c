5a2eb079340edabe9f134ecdf9a0695ed4fba8ad
METC <metc_ck1@hotmail.com>
2022-03-03 20:40:59 -0800

    1-string nconcat v4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * *string_nconcat - Conactenates 2 strings with n characters of 2nd string
- * @s1: First string
- * @s2: Second string
- * @n: n characters of second string
- *Return: char pointer to the concatenation allocated space
+ * string_nconcat - concatenates two strings
+ * @s1: char
+ * @s2: char
+ * @n: unsigned int
+ * Return: The returned pointer shall point to a newly allocated space in
+ * memory, which contains s1, followed by the first n bytes of s2, and null
+ * terminated. If the function fails, it should return NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, k = 0;
-char *con;
+	unsigned int i, j, size1, size2;
+	char *array;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-for (i = 0; s1[i]; i++)
-{
-}
-for (j = 0; s2[j] && j < n; j++)
-{
-}
-if (n > j)
-	con = malloc(sizeof(char) * (i + j + 1));
-if (j > n)
-	con = malloc(sizeof(char) * (i + n + 1));
-if (con == NULL)
-	return (NULL);
-for (k = 0; k < i; k++)
-{
-con[k] = s1[k];
-}
-for (k = 0 ; ((k < j) && (k < n)); k++)
-{
-	con[k + i] = s2[k];
-}
-con[k + i + 1] = '\0';
-return (con);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (size1 = 0 ; s1[size1] != '\0' ; size1++)
+	{
+	}
+	for (size2 = 0 ; s2[size2] != '\0' ; size2++)
+	{
+	}
+	if (n <= size2)
+	{
+	array = malloc(sizeof(char) * (size1 + n + 1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < size1 ; i++)
+	{
+		array[i] = s1[i];
+	}
+	for (j = 0 ; j < n ; j++)
+	{
+		array[i + j] = s2[j];
+	}
+	}
+	if (n > size2)
+	{
+	array = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < size1 ; i++)
+	{
+		array[i] = s1[i];
+	}
+	for (j = 0 ; j < size2 ; j++)
+	{
+		array[i + j] = s2[j];
+	}
+	}
+	array[i + j] = '\0';
+	return (array);
 }
