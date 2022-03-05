1382c46d4993a7522c098591d002bdd068cd65ff
METC <metc_ck1@hotmail.com>
2022-03-04 22:05:41 -0800

    100-realloc v2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,14 +16,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+		free (ptr);
+		return (newptr);
 	}
 	if (new_size > old_size)
 	{
