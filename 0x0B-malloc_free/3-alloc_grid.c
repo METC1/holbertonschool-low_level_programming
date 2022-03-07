a48291178b4140280b32bca334356e27434157aa
METC <metc_ck1@hotmail.com>
2022-03-06 17:18:23 -0800

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * **alloc_grid - Creattes 2D array reserving
+ * memmory with malloc and initializes array with 0
+ * @width: width of the array
+ * @height: height of the array
+ *Return: int pointer to the array
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **arr;
+
+if (width <= 0 || height <= 0)
+{
+	return (NULL);
+}
+arr = malloc(sizeof(int) * height);
+if (arr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+arr[i] = malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
+}
