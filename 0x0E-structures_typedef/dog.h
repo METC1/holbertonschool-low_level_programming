8ab3487cd9bf4f59f70a2d46df2b99360afd6cac
METC <metc_ck1@hotmail.com>
2022-03-08 21:55:13 -0800

    dog fixed pointer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog new type
+ * @name: parameter name of dog
+ * @age: parameter float age of dog
+ * @owner: parameter char name of owner
+ * Description: create a type DOg
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} *d;
+#endif
