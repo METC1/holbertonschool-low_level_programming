141be788799624d5119a86c75268879b5fe0193e
METC <metc_ck1@hotmail.com>
2022-03-09 21:51:59 -0800

    2-int index fix check null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,10 +10,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-int found;
 
-found = 0;
-if (size <= 0)
+if (size <= 0 || array == NULL || cmp == NULL)
 {
 	return (-1);
 }
@@ -22,12 +20,8 @@ for (i = 0; i < size; i++)
 	if (((*cmp)(array[i])) != 0)
 	{
 		return (i);
-		found = 1;
 	}
 }
-if (found == 0)
-{
-	return (-1);
-}
+
 return (-1);
 }
