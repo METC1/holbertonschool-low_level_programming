911b268907633d1e8313ae1753a441a804a6ad18
METC <metc_ck1@hotmail.com>
2022-03-12 20:29:02 -0800

    1-print numbers v1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - variadic function to print numbers
+ * @separator: char to separate  values 
+ * @n: unsigned int number of numbers to sum
+ * Return: void no return
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL || i == (n - 1))
+		{
+		printf("%u", va_arg(num, unsigned int));
+		}
+		else 
+		{
+			printf("%u%s", va_arg(num, unsigned int), separator);
+		}
+	}
+	printf("\n");
+	va_end(num);
+}
