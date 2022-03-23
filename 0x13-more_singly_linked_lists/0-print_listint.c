1e8aa744a19a7d6ad961c0254010c4bb9da8551a
METC <metc_ck1@hotmail.com>
2022-03-22 21:31:09 -0700

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint -  returns the number of elements in a linked list
+ * @h: pointer to the first node of a single linked list
+ * Return: size_t type
+ */
+size_t print_listint(const list_t *h)
+{
+	size_t i = 0;
+
+	for ( ; h != NULL ; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
