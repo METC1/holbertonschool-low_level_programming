903e238579c8783bdc721cba66c8ff0460a7978f
METC <metc_ck1@hotmail.com>
2022-03-22 22:15:47 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node at the beginning of a list_t list
+ * containing the duplicated str
+ * @head: pointer to the pointer pointing the first element of
+ * the first node of a single linked list
+ * @n: value of n 
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+	size_t nodo = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+	}
+}
