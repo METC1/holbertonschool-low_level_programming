8b3b0a6e13d02868384f86de090d62751ec9d358
METC <metc_ck1@hotmail.com>
2022-03-22 22:39:51 -0700

    3 add node int end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - adds a new node at the end of a list_t list
+ * containing the duplicated str
+ * @head: pointer to the pointer pointing the first element of
+ * the first node of a single linked list
+ * @n: n value
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodent_end(listnt_t **head, const int n)
+{
+	list_t *new, *temp;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		for ( ; temp->next != NULL ;)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	return (*head);
+}
