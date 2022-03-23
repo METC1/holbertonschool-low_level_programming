495c33e238af31cc308e3c510ec768f81a64bc99
METC <metc_ck1@hotmail.com>
2022-03-22 21:52:06 -0700

    list int len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint_len -  returns the number of elements in a linked list
+ * @h: pointer to the first node of a single linked list
+ * Return: size_t type
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	for ( ; h != NULL ; i++)
+	{
+		/*printf("%d\n", h->n);*/
+		h = h->next;
+	}
+	return (i);
+}
