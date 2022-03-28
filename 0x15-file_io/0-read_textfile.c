c9582188062b2dcdc94ae43d210cba9a5beea2ca
METC <metc_ck1@hotmail.com>
2022-03-28 12:48:56 -0700

    first upload 0 read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * read_textfile - Reads the specified bytes of a text file
+ * @filename: filename of file to read
+ * @letters: number of letters to read
+ * Return: ssize_t number of letters read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t chrrd;
+int fd;
+char *buffer;
+
+if (filename == NULL)
+	return (0);
+buffer = malloc(letters + 1);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+	return (0);
+chrrd = read(fd, buffer, letters);
+buffer[letters + 1] = '\0';
+printf("%s", buffer);
+close(fd);
+return (chrrd);
+}
