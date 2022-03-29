aac4fd9bae36a86503e707cd43c648c74b930313
METC <metc_ck1@hotmail.com>
2022-03-28 21:25:59 -0700

    fixed truncating

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int fd, chars;
 chars = 0;
 if (filename == NULL)
 	return (-1);
-fd = open(filename, O_WRONLY | O_CREAT, 0600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 	return (-1);
 if (text_content == NULL)
