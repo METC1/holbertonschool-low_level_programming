08e1a3c307e525a44e2d7e79e530678a137b35a3
METC <metc_ck1@hotmail.com>
2022-05-06 19:49:55 -0700

    print double linked list

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,90 +1,79 @@
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-int _putchar(char c);
-char *buffer(char *filename);
-
+#include "main.h"
 /**
- * buffer - function buffer of 1024 bytes
- * @filename: name of ile
- * Return: Always 0.
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if succes
  */
-char *buffer(char *filename)
+int main(int argc, char *argv[])
 {
-	char *buffr;
+	int fd_from, fd_to, nw_to, nr_from, i, k;
+	char *ptr;
 
-	buffr = malloc(sizeof(char) * 1024);
-	if (buffr == NULL)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	ptr = malloc(sizeof(char) * 1024);
+	if (ptr == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	nr_from = read(fd_from, ptr, 1024);
+	if (nr_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+	if (fd_to < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	return (buffr);
-}
-
-
-
-
-/**
- * main - copy files
- * @args: number of parameters
- * @av: array (1)file to copy, (2)destination file
- * Return: Always 0.
- */
-int main(int args, char **av)
-{
-	int fd, chrrd, chrwr, chr, clse;
-	char *buffr;
-
-fd = 0;
-chrrd = 0;
-chrwr = 0;
-chr = 0;
-
-if (args != 3)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
-}
-buffr = buffer(av[2]);
-	fd = open(av[1], O_RDONLY);
-	chrrd = read(fd, buffr, 1024);
-	chr = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	while (chrrd > 0)
+	k = nr_from;
+	for ( ; nr_from > 0 ;)
 	{
-		if (fd == -1 || chrrd == -1)
+		nw_to = write(fd_to, ptr, nr_from);
+		if (nw_to < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-		chrwr = write(chr, buffr, chrrd);
-		if (chr == -1 || chr != chrrd)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-		chrrd = read(fd, buffr, 1024);
-		chr = open(av[2], O_WRONLY | O_APPEND);
-	} while (chrrd > 0);
-	free(buffr);
-	clse = close(fd);
-	if (clse == -1)
+		nr_from = read(fd_from, ptr, 1024);
+		k += nr_from;
+		if (nr_from < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+	}
+	nr_from = k;
+	i = close(fd_from);
+	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-	clse = close(chr);
-	if (clse == -1)
+	i = close(fd_to);
+	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", chr);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-	if (chrwr == 0)
-		return (0);
-return (0);
+	free(ptr);
+	return (0);
 }
