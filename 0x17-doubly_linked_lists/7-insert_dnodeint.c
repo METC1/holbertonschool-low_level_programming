67d4d5a73d9cdf92a326de7a9fb4b8bb2a6010c2
METC <metc_ck1@hotmail.com>
2022-05-06 20:50:23 -0700

    7-insert node fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,23 +1,48 @@
 #include "lists.h"
 /**
- * sum_dlistint - returns the sum of all the data (n) of a double linked list
- * @head: head of list
- * Return: returns the result of the sum of all the n data
+ * insert_dnodeint_at_index -  inserts a new node at a given position
+ * @h: head of list
+ * @idx: node position, that starts at 0.
+ * @n: data of the inserted node
+ * Return: returns a pointer to the idx node of a dlistint_t linked list
  */
-int sum_dlistint(dlistint_t *head)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = head;
-	int i = 0, j = 0;
+	dlistint_t *new = NULL, *current = *h, *before = NULL;
+	unsigned int i = 0;
 
-	if (head == NULL)
+	if (*h == NULL && idx != 0)
 	{
-		return (i);
+		return (NULL);
 	}
-	for ( ; temp != NULL ; )
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 	{
-		j = temp->n;
-		i += j;
-		temp = temp->next;
+		return (NULL);
 	}
-	return (i);
+	new->next = NULL;
+	new->n = n;
+	new->prev = NULL;
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	for (i = 0 ; current != NULL ; i++)
+	{
+		if (i == idx)
+		{
+			new->next = current;
+			new->prev = before;
+			before->next = new;
+			current->prev = new;
+			return (new);
+		}
+		before = current;
+		current = current->next;
+	}
+	if (i == idx)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	return (NULL);
 }
