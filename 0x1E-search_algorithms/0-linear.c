50d836dca1f61f495a120baf3bf739f64fb78ebd
METC <metc_ck1@hotmail.com>
2022-08-29 09:21:16 -0700

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,10 +21,9 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (i = 0; i <= size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found %d at index %d\n", value, i);
 			ret = i;
 			return (ret);
 		}
