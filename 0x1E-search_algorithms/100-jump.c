3b636a7385115358b3fe4e82729f3f44f152ccca
METC <metc_ck1@hotmail.com>
2022-09-02 15:46:03 -0700

    100-jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,27 +11,26 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	int left = 0, right = 0, step = sqrt((int) size);
-
-	for (; array != NULL && left < (int) size ;)
+int left = 0, right = 0, step = sqrt((int) size);
+for (; array != NULL && left < (int) size ;)
 	{
-		if (value <= array[right] || right >= (int) size)
+	if (value <= array[right] || right >= (int) size)
 		{
-			printf("Value found between indexes [%d] and [%d]\n", left, right);
-			for (; left <= right ; left++)
+		printf("Value found between indexes [%d] and [%d]\n", left, right);
+		for (; left <= right ; left++)
 			{
-				printf("Value checked array[%d] = [%d]\n", left, array[left]);
-				if (array[left] == value)
-					return (left);
-				if (array[left] == ((int) size - 1) || value < array[left + 1])
-					return (-1);
+			printf("Value checked array[%d] = [%d]\n", left, array[left]);
+			if (array[left] == value)
+			return (left);
+			if (array[left] == ((int) size - 1) || value < array[left + 1])
+			return (-1);
 			}
 		}
 		else
 		{
-			printf("Value checked array[%d] = [%d]\n", right, array[right]);
-			left = right;
-			right += step;
+		printf("Value checked array[%d] = [%d]\n", right, array[right]);
+		left = right;
+		right += step;
 		}
 	}
 	return (-1);
